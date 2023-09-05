cf9c0e195c8185bc5c964986cb46fcb5b3bd9a12
MariyqZlateva <mimitooo4@gmail.com>
2023-09-05 23:06:47 +0300

    Added single responsibilities

---
+++ SingleResponsibilityExample/src/com/zlateva/Finance.java
@@ -0,0 +1,9 @@
+package com.zlateva;
+
+public class Finance {
+
+    public double calculateIncomeTaxForCurrentYear(Employee emp) {
+        // income tax logic using employee passed in
+        return 0;
+    }
+}
---
+++ SingleResponsibilityExample/src/com/zlateva/HRPromotions.java
@@ -0,0 +1,10 @@
+package com.zlateva;
+
+public class HRPromotions {
+
+    public boolean isPromotionDueThisYear(Employee emp){
+        // promotion logic implementation is using the employee information passed in
+        return true;
+    }
+
+}
