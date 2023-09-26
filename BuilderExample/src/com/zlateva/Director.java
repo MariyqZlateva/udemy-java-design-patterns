44da5b69bcd84308ab253fe30a4e0742c2b8fb4d
MariyqZlateva <mimitooo4@gmail.com>
2023-09-26 12:37:28 +0300

    Added Builder example

---
+++ BuilderExample/src/com/zlateva/Director.java
@@ -0,0 +1,11 @@
+package com.zlateva;
+
+public class Director {
+    BuilderInterface myBuilder;
+    public void construct(BuilderInterface builder){
+        myBuilder = builder;
+        myBuilder.buildBody();
+        myBuilder.insertWheels();
+        myBuilder.addHeadlights();
+    }
+}
