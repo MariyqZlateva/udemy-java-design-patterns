0104ac87907352f65bb28c3e750cd7ce0689b882
MariyqZlateva <mimitooo4@gmail.com>
2023-10-03 15:34:26 +0300

    Added Adapter challenge

--- Adapter_challenge/src/com/zlateva/MediaPlayer.java
+++ Adapter_challenge/src/com/zlateva/MediaPlayer.java
@@ -9,7 +9,7 @@ class AudioPlayer implements MediaPlayer{
     @Override
     public void play(String audioType, String fileName) {
         if (audioType.equalsIgnoreCase("mp3")){
-            System.out.println("Playing mp3 file. Name :" + fileName);
+            System.out.println("Playing mp3 file. Name : " + fileName);
         }else {
             System.out.println("Invalid media "+ audioType + " format not supported");
         }
