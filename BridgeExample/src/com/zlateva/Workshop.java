ce63951a9cd8f8e851b7719b02482a44d8393b5f
MariyqZlateva <mimitooo4@gmail.com>
2023-10-05 14:49:08 +0300

    Added Bridge example

---
+++ BridgeExample/src/com/zlateva/Workshop.java
@@ -0,0 +1,20 @@
+package com.zlateva;
+
+public abstract class Workshop {
+    abstract public void work();
+
+}
+
+class Produce extends Workshop{
+    @Override
+    public void work() {
+        System.out.println("Produced");
+    }
+}
+
+class Assemble extends Workshop{
+    @Override
+    public void work() {
+        System.out.println("And Assembled");
+    }
+}
