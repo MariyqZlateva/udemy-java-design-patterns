af3285a04e46b8032061ac171d7521b36a705113
MariyqZlateva <mimitooo4@gmail.com>
2023-10-10 10:15:34 +0300

    Added Decorator Challenge

---
+++ DecoratorChallenge/src/com/zlateva/Shape.java
@@ -0,0 +1,19 @@
+package com.zlateva;
+
+public interface Shape {
+    void draw();
+}
+
+class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
+
+class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+}
---
+++ DecoratorChallenge/src/com/zlateva/ShapeDecorator.java
@@ -0,0 +1,48 @@
+package com.zlateva;
+
+public abstract class ShapeDecorator implements Shape {
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    @Override
+    public void draw() {
+        if (decoratedShape != null) {
+            decoratedShape.draw();
+        }
+    }
+}
+
+class RedShapeDecorator extends ShapeDecorator {
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setBorder(decoratedShape);
+    }
+
+    private void setBorder(Shape decoratedShape){
+        System.out.println("Border Color: Red");
+    }
+}
+
+class GreenShapeDecorator extends ShapeDecorator {
+    public GreenShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setBorder(decoratedShape);
+    }
+
+    private void setBorder(Shape decoratedShape){
+        System.out.println("Border Color: Green");
+    }
+}
