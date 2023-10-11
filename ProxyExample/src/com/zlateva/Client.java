644a1fc841a93db4eded88e907fe2dddaa9d4f63
MariyqZlateva <mimitooo4@gmail.com>
2023-10-11 13:14:54 +0300

    Added Proxy Exercise

---
+++ ProxyExample/src/com/zlateva/Client.java
@@ -0,0 +1,12 @@
+package com.zlateva;
+
+public class Client {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("someFilename.jpg");
+
+        // image will be loaded from disk
+        image.display();
+        System.out.println(" ");
+        image.display();
+    }
+}
