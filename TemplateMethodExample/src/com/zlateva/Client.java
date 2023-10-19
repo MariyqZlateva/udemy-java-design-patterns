eb95aba348a9db8dc261c0e438914a8ab30409bb
MariyqZlateva <mimitooo4@gmail.com>
2023-10-19 15:41:39 +0300

    Added Template method example

---
+++ TemplateMethodExample/src/com/zlateva/Client.java
@@ -0,0 +1,14 @@
+package com.zlateva;
+
+public class Client {
+    public static void main(String[] args) {
+        HouseTemplate houseType = new WoodenHouse();
+
+        //using template method
+        houseType.buildHouse();
+        System.out.println("***************************");
+
+        houseType = new GlassHouse();
+        houseType.buildHouse();
+    }
+}
